abd14f59bf4873c2d0715e5a5e1c551882a9e4fe
nddjdjhdhr <cloudways835@gmail.com>
2025-04-15 21:49:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_TOKEN = "7781064101:AAGwS43iWHqTobGvJXOQqTKYtbUxN6hh_JI"  # Get from @BotFather
+ADMIN_ID = ["8179218740"]      # Your Telegram user ID
+CHANNEL_USERNAME = "@mustafaleaks2"  # Channel users must join
+FEEDBACK_CHANNEL = "@feedbackchannel47"  # Where screenshots go
+ALLOWED_GROUPS = ["-1004647976008"]  # Default group ID
